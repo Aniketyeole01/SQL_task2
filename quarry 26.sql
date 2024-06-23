5fef09cf6081759a4160236cb0da2c8a97267657
Aniketyeole01 <aniketyeole200@gmail.com>
2024-06-23 16:47:18 +0530

    first commit

---
+++ quarry 26.sql
@@ -0,0 +1,48 @@
+create table student (
+	id int,
+	name varchar,
+	price int,
+	status boolean
+)
+
+select * from student
+
+insert into student values (1,'s1',45,true)
+
+select * from student
+
+insert into student values (2,'s2',34,false)
+
+select * from student
+
+insert into student values (3,'rohan',50,false)
+
+select * from student
+
+insert into student values (4,'prajwal',53,true)
+
+select * from student
+
+insert into student values (5,'nikhil',55,true)
+
+select * from student
+
+insert into student values (6,'satish',58,false)
+
+select * from student
+
+insert into student values (10,'athrva',66,false)
+
+select * from student 
+
+insert into student values (12,'atul',56,true)
+
+select * from student 
+
+insert into student values (13,'jaiswal',70,true)
+
+select * from student 
+
+insert into student values (14,'purohit',75,false)
+
+select * from student
